72ad79443745db1bce97fe34a2196781b04d845e
Mateusz Żółtak <zozlak@zozlak.org>
2024-12-17 12:24:35 +0100

    LogMultiplexer class added

---
+++ src/zozlak/logging/LogMultiplexer.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2024 zozlak.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace zozlak\logging;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+/**
+ * Description of LogMultiplexer
+ *
+ * @author zozlak
+ */
+class LogMultiplexer extends \Psr\Log\AbstractLogger {
+
+    /**
+     * 
+     * @var array<LoggerInterface>
+     */
+    private array $logs;
+
+    public function addLog(LoggerInterface $log): self {
+        $this->logs[] = $log;
+        return $this;
+    }
+
+    public function log($level, $message, mixed $context = []): void {
+        foreach ($this->logs as $i) {
+            $i->log($level, $message, $context);
+        }
+    }
+}
